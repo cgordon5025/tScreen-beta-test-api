ecbacc12d1045cf3a5726012929a774ebe06cfab
unknown <cgordon5025@gmail.com>
2023-04-26 09:45:08 -0400

    dataloaders and extensions

---
+++ tScreen/tScreen.Api/GraphQl/DataLoaders/LocationByIdDataLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Extensions;
+using Data;
+using GreenDonut;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQl.GraphQl.DataLoaders
+{
+    public class LocationByIdDataLoader : BatchDataLoader<Guid, Models.Location>
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+        private readonly IMapper _mapper;
+
+        public LocationByIdDataLoader(
+            IDbContextFactory<ApplicationDbContext> contextFactory,
+            IMapper mapper,
+            IBatchScheduler batchScheduler,
+            DataLoaderOptions? options = null)
+            : base(batchScheduler, options)
+        {
+            _contextFactory = contextFactory;
+            _mapper = mapper;
+        }
+
+        protected override async Task<IReadOnlyDictionary<Guid, Models.Location>> LoadBatchAsync(
+            IReadOnlyList<Guid> keys,
+            CancellationToken cancellationToken)
+        {
+            await using var context = await _contextFactory.CreateDbContextAsync(cancellationToken);
+
+            return await context.Location
+                .TagWith($"{nameof(LocationByIdDataLoader)}.{nameof(LoadBatchAsync)}")
+                .TagWithCallSiteSafely()
+                .Where(e => keys.Contains(e.Id))
+                .Select(e => _mapper.Map<Models.Location>(e))
+                .ToDictionaryAsync(e => e.Id, cancellationToken);
+        }
+    }
+}
---
+++ tScreen/tScreen.Api/GraphQl/DataLoaders/SessionDataByIdDataLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Extensions;
+using Data;
+using GraphQl.GraphQl.Models;
+using GreenDonut;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQl.GraphQl.DataLoaders;
+
+public class SessionDataByIdDataLoader : BatchDataLoader<Guid, Session>
+{
+
+    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+    private readonly IMapper _mapper;
+
+    public SessionDataByIdDataLoader(
+        IDbContextFactory<ApplicationDbContext> contextFactory,
+        IMapper mapper,
+        IBatchScheduler batchScheduler,
+        DataLoaderOptions? options = null)
+        : base(batchScheduler, options)
+    {
+        _contextFactory = contextFactory;
+        _mapper = mapper;
+    }
+
+    protected override async Task<IReadOnlyDictionary<Guid, Session>> LoadBatchAsync(
+        IReadOnlyList<Guid> keys,
+        CancellationToken cancellationToken)
+    {
+        await using var context = await _contextFactory.CreateDbContextAsync(cancellationToken);
+
+        return await context.AppSessions
+            .TagWith($"{nameof(SessionDataByIdDataLoader)}.{nameof(LoadBatchAsync)}")
+            .TagWithCallSiteSafely()
+            .Where(e => keys.Contains(e.Id))
+            .Select(e => _mapper.Map<Session>(e))
+            .ToDictionaryAsync(e => e.Id, cancellationToken);
+    }
+}
